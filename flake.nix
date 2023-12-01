a5459568e0d31ac20481d8a01188e90ab3efcd67
Kevin Rangel <code@venikx.com>
2023-12-01 16:56:05 +0100

    chore: setup flakes

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "Advent of Code";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    (flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [ go gopls gotools go-tools ];
+        };
+      }));
+}
